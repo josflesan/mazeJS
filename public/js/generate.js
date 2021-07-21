132401138661543c63b2a253099bfbe21e5deb40
Joflesan <s2088605@ed.ac.uk>
2021-07-21 08:28:26 +0100

    Functioning Grid Size Dropdown + Refactor

--- public/js/generate.js
+++ public/js/generate.js
@@ -21,18 +21,17 @@ import { Algorithms } from './components/algorithms.js'
         ctx.canvas.height = container.height;
         ctx.canvas.width = canvas.height;
 
-        gridSize = 30  // 30x30 grid
+        // ************ DEFAULT MAZE SETTINGS ************
 
+        gridSize = 5  // 30x30 grid
         cellSize = canvas.height/gridSize
-
         grid = new Grid(gridSize, cellSize, canvas.height, canvas.width)
+        let startCell = openMaze()  // Remove start and end walls of maze to open it
+        
+        // ************************************************
 
-        // Declare starting cell, remove wall 
-        let startCell = grid.getRandom()
-        grid.getCell(0, 0).deleteWall("left", grid.getCell(0, 0).getNeighbours(grid))  // Delete wall from start cell
-        let endCell = grid.getCell(gridSize-1, gridSize-1)
-        endCell.deleteWall("right", endCell.getNeighbours(grid))
-
+        gridSizeChange()  // Change size of grid
+        
         grid.draw(ctx, false)
         
         // Implement play button functionality
@@ -50,16 +49,59 @@ import { Algorithms } from './components/algorithms.js'
 
     }
 
+    /**
+     * Function that updates the screen with the updated maze state
+     * @param {Boolean} color Whether or not the cells will be coloured in according to their state 
+     */
     function update(color) {
         // Clear grid before re-drawing
         ctx.clearRect(0, 0, grid.totalWidth, grid.totalHeight)
         grid.draw(ctx, color)
     }
 
-    function onClickButton() {
+    /**
+     * Function that handles the change in grid size as selected
+     * by the drop-down menu
+     */
+    function gridSizeChange() {
+        let sizeList = document.getElementById("gridSizeList")
         
+        sizeList.onchange = function() {
+            let tagContent = sizeList.options[sizeList.selectedIndex].innerHTML
+            let size = parseInt(tagContent.substring(0, 2).trim())  // Remove spaces for single digit numbers
+            gridSize = size
+            cellSize = canvas.height / size
+
+            grid = new Grid(gridSize, cellSize, canvas.height, canvas.width)
+            ctx.clearRect(0, 0, canvas.width, canvas.height)
+            
+            openMaze()
+            grid.draw(ctx, false)
+        }
     }
 
+    /**
+     * Function that removes the starting cell's left wall and the ending
+     * cell's right wall in the maze so that the maze is solvable from left
+     * to right.
+     * @returns {Cell} The starting cell object passed as a parameter to the DFS algorithm  
+     */
+    function openMaze() {
+        // Declare starting cell, remove wall 
+        let startCell = grid.getRandom()
+        grid.getCell(0, 0).deleteWall("left", grid.getCell(0, 0).getNeighbours(grid))  // Delete wall from start cell
+        // Declare ending cell, remove wall
+        let endCell = grid.getCell(gridSize-1, gridSize-1)
+        endCell.deleteWall("right", endCell.getNeighbours(grid))
+
+        return startCell
+    }
+
+    /**
+     * Function that handles the logic behind the play and stop buttons
+     * in terms of switching sprites and handling the running behaviour of the
+     * selected algorithm
+     */
     function changeButton() {
         if (buttonState == "PAUSED") {
             buttonState = "PLAY"
