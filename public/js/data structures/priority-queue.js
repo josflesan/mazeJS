3f263c4e20e9b73ba7b0d1e59bc8f604ab3225bc
Joflesan <s2088605@ed.ac.uk>
2021-10-14 07:56:13 +0100

    Refactored Priority Queue Class

--- public/js/data structures/priority-queue.js
+++ public/js/data structures/priority-queue.js
@@ -11,44 +11,94 @@ class PriorityQueue {
     }
 
     /**
-     * Function that heapifies the tree if not already heapified.
-     * A heapified tree satisfies the heap invariant such that every
-     * parent node in the tree is ordered with respect to its children
-     * @param {Integer} i       The position of the root node in the items array
+     * Perform a down-heap or heapify-down operation for a min heap
+     * @param {Integer} i   The index to start at when heapifying down
      */
-    heapify(i) {
-        let lengthQueue = this._items.length
-
-        // Find the smallest among the roots, left child and right child
+    minHeapifyDown(i) {
+        let leftChild = 2*i
+        let rightChild = 2*i + 1
         let smallest = i
-        let leftChild = 2 * i + 1
-        let rightChild = 2 * i + 2
 
-        if (leftChild < lengthQueue && this._items[smallest].fScore > this._items[leftChild].fScore) {
+        if (leftChild < this._items.length && 
+            this._items[leftChild].fScore < this._items[smallest].fScore) {
             smallest = leftChild
         }
 
-        if (rightChild < lengthQueue && this._items[smallest].fScore > this._items[rightChild].fScore) {
+        if (rightChild < this._items.length && 
+            this._items[rightChild].fScore < this._items[smallest].fScore) {
             smallest = rightChild
         }
 
-        // Swap and continue heapifying if the root is not the largest
         if (smallest != i) {
+            // Swap items[i] and items[smallest]
             let temp = this._items[i]
             this._items[i] = this._items[smallest]
             this._items[smallest] = temp
-            this.heapify(smallest)
+
+            this.minHeapify(smallest)
         }
     }
 
     /**
+     * Function that sorts the tree up if it's not heapified.
+     * A heapified tree satisfies the heap invariant such that every
+     * parent node in the tree is ordered with respect to its children
+     * @param {Integer} i       The position of the new node in the items array
+     */
+    minHeapifyUp(i) {
+        // let lengthQueue = this._items.length
+        let parentIndex = Math.floor((i - 1) / 2)
+        let itemIndex = i
+
+        while (true) {
+            let parentItem = this._items[parentIndex]
+            let item = this._items[itemIndex]
+            if (item.fCost < parentItem.fCost) 
+            {
+                // Swap item with parent item
+                this._items[parentIndex] = item
+                this._items[itemIndex] = parentItem
+                itemIndex = parentIndex
+            }
+            else {
+                break
+            }
+            parentIndex = Math.floor((parentIndex - 1) / 2)
+            if (parentIndex < 0) {
+                break
+            }
+        }
+
+        // // Find the smallest among the roots, left child and right child
+        // let smallest = i
+        // let leftChild = 2 * i + 1
+        // let rightChild = 2 * i + 2
+
+        // if (leftChild < lengthQueue && this._items[smallest].fScore > this._items[leftChild].fScore) {
+        //     smallest = leftChild
+        // }
+
+        // if (rightChild < lengthQueue && this._items[smallest].fScore > this._items[rightChild].fScore) {
+        //     smallest = rightChild
+        // }
+
+        // // Swap and continue heapifying if the root is not the largest
+        // if (smallest != i) {
+        //     let temp = this._items[i]
+        //     this._items[i] = this._items[smallest]
+        //     this._items[smallest] = temp
+        //     this.heapify(smallest)
+        // }
+    }
+
+    /**
      * Function that pops the minimum value from the PQ
-     * @return {Object} The root node of the PQ
+     * @return {Cell} The root node of the PQ
      */
     popHeap() {
         let lengthPQ = this._items.length
 
-        // Save the head of the PQ
+        // Save the head of the PQ and reduce current item count
         let headPQ = this._items[0]
 
         // Swap last leaf and root node
@@ -60,7 +110,7 @@ class PriorityQueue {
         this._items.pop()
 
         // Repair downward
-        this.heapify(0)
+        this.minHeapifyDown(0)
 
         return headPQ
     }
@@ -68,26 +118,39 @@ class PriorityQueue {
     /**
      * Function that returns the smallest value in the PQ without
      * popping it from the Priority Queue
-     * @return {Object}  The object at the root of the heap
+     * @return {Cell}  The object at the root of the heap
      */
     peek() {
         return this._items[0]
     }
 
     /**
+     * Function that checks if the list contains a particular element
+     * @param {Cell} element    The element we are searching for
+     * @return {Boolean}        Whether or not the element was found in the PQ
+     */
+    contains(element) {
+        this._items.forEach((el) => {
+            if (el == element) {
+                return true
+            }
+        })
+
+        return false
+    }
+
+    /**
      * Function that inserts an element into the priority queue, 
      * making sure to heapify the tree after doing so
-     * @param {Any} newElement      The new element being inserted into the PQ
+     * @param {Cell} newElement      The new element being inserted into the PQ
      */
     insert(newElement) {
         if (this._items.length == 0) {
-            this._items.push(newElement)
+            this._items.push(newElement)  // Place item at root
         } 
         else {
             this._items.push(newElement)
-            for (let i = (this._items.length - 1) / 2; i > -1; i--) {
-                this.heapify(this._items, this._items.length, i)
-            }
+            this.minHeapifyUp(this._items.length - 1)
         }
     }
 
@@ -112,9 +175,7 @@ class PriorityQueue {
 
         this._items.pop()  // Remove the last element in the PQ
 
-        for (let i = (lengthPQ / 2) - 1; i > -1; i--) {
-            this.heapify(this._items, this._items.length, i)
-        }
+        this.minHeapifyDown(pos)  // Pass the index of the new item
     }
     
     /**
